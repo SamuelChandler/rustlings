c6cf3a023f3905c669af4a674ff65897de114452
SamuelChandler <sec180005@utdallas.edu>
2024-03-04 14:00:05 -0600

    progressing in strings

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -7,17 +7,18 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    &input.append(" world!");
+    input.to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    
 }
 
 #[cfg(test)]
